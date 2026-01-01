c2041e6feb024c59ed985250ce331ea1e8abe0c5
pa1kumarCSML <dpavankumar8797@gmail.com>
2026-01-01 12:46:14 +0530

    rendering elements in react components, rendering components inside another component

--- App.js
+++ App.js
@@ -1,23 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
 // react element
-const jsxHeading = (<h1 id="child">hello world from jsx</h1>);
+const jsxHeading = <h1 id="child">hello world from jsx</h1>;
 
 //react components
 // -> class based - old way
 // -> functional based - new way
 
-
 // functional react component(new way)
-const HeadingComponent = () => (<h1 className="heading">Heading Component</h1>); // mostly used syntax
+
+//note: we can normal functions like js function to return react component
+
+// react element
+const JsxHeading2 = () => <h1 id="child">hello world from child component</h1>;
+
+const HeadingComponent = () => (
+  <div>
+    <JsxHeading2 />
+    {jsxHeading}
+    <h1 className="heading">Heading Component</h1>
+  </div>
+); // mostly used syntax
 
 const HeadingComponent2 = () => {
-    return (
-        <h1 className="heading">Heading Component</h1>
-    )
-};// more readable syntax
+  return <h1 className="heading">Heading Component</h1>;
+}; // more readable syntax
 
 const HeadingComponent3 = () => <h1 className="heading">Heading Component</h1>; // only one element and also in one line
 
