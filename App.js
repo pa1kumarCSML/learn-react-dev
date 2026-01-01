61d286a856471b6096d531e7aa1ede842c0f0764
pa1kumarCSML <dpavankumar8797@gmail.com>
2026-01-01 12:06:27 +0530

    JSX intro

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+
+
+const jsxHeading = <h1 id="child">hello world from jsx</h1>;
+
 const parent = React.createElement("div", { id: "parent" },
     [
         React.createElement("h1", { id: "child" }, "hello world from react"),
-        React.createElement("h1", { id: "child" }, "hello world from react")
+        jsxHeading
     ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
